31d4cf54475cdb33f1935fccd05c3035f7abbfd6
Azhar <marmuazhar@gmail.com>
2023-08-15 16:13:26 +0530

    task-4 solution

--- dart/task-solution/task4.dart
+++ dart/task-solution/task4.dart
@@ -14,11 +14,46 @@
 // in first, fourth and fifth row has 5 boxes => (2,2,1,2,1)
 // in second and third row has 4 boxes => (2,2,2,2)
 
-
 // search how many tablets in the box
 //1. Rack number (1, 2, 3)
 //2. Row number
 //3. Box number
 //print('Tablets: ')
 
-void main() {}
+import 'dart:io';
+
+void main() {
+  var racks = [
+    [
+      [2, 2, 2, 1],
+      [2, 2, 2, 2, 2],
+      [1, 2, 1, 2, 1, 2],
+      [1, 2, 1, 2, 1, 2],
+    ],
+    [
+      [2, 2, 1],
+      [2, 2, 7, 2],
+      [2, 2, 1],
+      [2, 2, 1],
+    ],
+    [
+      [2, 2, 1, 2, 1],
+      [2, 2, 2, 2],
+      [2, 2, 2, 2],
+      [2, 2, 1, 2, 1],
+      [2, 2, 1, 2, 1],
+    ],
+  ];
+
+  print('Enter your RackNumber: ');
+  int rackNumber = int.parse(stdin.readLineSync()!);
+
+  print('Enter your Row Number: ');
+  int rowNumber = int.parse(stdin.readLineSync()!);
+
+  print('Enter your Box Number: ');
+  int boxNumber = int.parse(stdin.readLineSync()!);
+
+  int tablets = racks[rackNumber - 1][rowNumber - 1][boxNumber - 1];
+  print("Tablets: $tablets");
+}
