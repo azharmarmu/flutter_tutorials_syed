7316d0b1d9c402da4f0743850bd5e6eb23721251
Azhar <marmuazhar@gmail.com>
2023-08-21 20:57:42 +0530

    task5-try carch

---
+++ dart/task-solution/task5.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main() {
+  List<String> carBrands = ['maruthi', 'toyota', 'kia', 'tata'];
+  print("Enter an index number:");
+
+  String? input = stdin.readLineSync(); // get input through terminal/console
+  String validInput = input!; //conversion1=> String? to String
+ 
+  try {
+    int index = int.parse(validInput); //conversion2 => String to Integer
+    print('index: $index');
+    if (index >= 0 && index <= carBrands.length - 1) {
+      print("Car brand at index $input: ${carBrands[index]}");
+    } else {
+      print("Not found");
+    }
+  } catch (e) {
+    print('Enter proper value');
+  }
+
+  // Line 7,8,9 can be simplied into this ==> int index = int.parse(stdin.readLineSync()!)
+
+  print('\n\n---Task5 End---');
+}
